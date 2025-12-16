9d19b98697085a88177dfdfb770d3e8b47560f40
FutureSeth <seth@thefuturesapp.com>
2025-12-16 14:47:18 -0700

    Updating package req.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Aespa",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS("17.0")],
     products: [
         .library(
             name: "Aespa",
